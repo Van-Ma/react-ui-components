63c381188e7535b7dd71d7357ddcfb108bd4fbc4
Van Ma <117691848+vanmaa@users.noreply.github.com>
2024-11-05 16:14:30 -0600

    Create FishBowl.js

---
+++ FishBowl/FishBowl.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './fishbowl.css';
+
+const Fishbowl = () => {
+  const [rotation, setRotation] = useState(0);
+  const fishRef = useRef(null); 
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const fishbowl = document.querySelector('.fishbowl');
+      const fishbowlRect = fishbowl.getBoundingClientRect();
+      const fishbowlCenterX = fishbowlRect.left + fishbowlRect.width / 2;
+      const fishbowlCenterY = fishbowlRect.top + fishbowlRect.height / 2;
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      let distanceY = mouseY - fishbowlCenterY;
+      const totalDistance = fishbowlRect.height / 2;
+
+      if (mouseX <= fishbowlCenterX) {
+        setIsFlipped(true); 
+        distanceY = - distanceY; 
+      } else {
+        setIsFlipped(false);
+      }
+
+      const rotationRadians = Math.asin(distanceY / totalDistance);
+      let rotationDegrees = rotationRadians * (180 / Math.PI); 
+
+
+      setRotation(rotationDegrees);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+  return (
+    <div className="fishbowl-container">
+      <div className="fishbowl">
+        <div className="fish">
+          <img
+            ref={fishRef} src="goldfish.png" alt="Goldfish"
+            style={{
+              transform: `rotate(${rotation}deg) ${isFlipped ? 'scaleX(-1)' : 'scaleX(1)'}`,
+              transition: 'transform 3s ease',
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Fishbowl;
